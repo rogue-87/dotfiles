f3d35fced24f3a55b64b3bcc196978b75ccc261e
Rogue <rogue87.dev@gmail.com>
2025-01-06 12:24:46 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,44 +5,35 @@ sudo dnf copr enable atim/lazygit -y
 
 ## packages list
 packages=(
-  kitty
-  neovim
-  fastfetch
-  stow
-  btop
-  cava
-  lazygit
-  npm
-  nodejs
-  python3
-  pip
-  lua
-  luarocks
-  rust
-  fish
+	kitty
+	neovim
+	fastfetch
+	stow
+	btop
+	cava
+	lazygit
+	npm
+	nodejs
+	python3
+	pip
+	lua
+	luarocks
+	rust
+	fish
 )
 
 ## npm packages(required by neovim plugins)
 npm_g_packages=(
-  all-the-package-names
-  browser-sync
-  neovim
-  pnpm
-  yarn
+	all-the-package-names
+	browser-sync
+	neovim
+	pnpm
+	yarn
 )
-
-## set up path for global npm packages
-PREFIX="$HOME/.npm/packages"
-if [ ! -d "$PREFIX" ]; then
-  echo "Creating prefix directory: $PREFIX"
-  mkdir -p "$PREFIX"
-fi
-npm config set prefix "$PREFIX"
-echo "Global npm prefix set to: $PREFIX"
 
 # install packages
 sudo dnf install "${packages[@]}"
-npm i -g "${npm_g_packages[@]}"
+sudo npm i -g "${npm_g_packages[@]}"
 
 # bak up .bashrc just in case :)
 mv $HOME/.bashrc $HOME/.bashrc.bak
