d5326683ce7746bf33465fbebbb32249f7a331d5
Rogue <rogue87@tuta.io>
2025-04-26 16:55:41 +0200

    add focus.nvim

--- profiles/laptop/nvim/.config/nvim/lua/plugins/editor/etc.lua
+++ profiles/laptop/nvim/.config/nvim/lua/plugins/editor/etc.lua
@@ -18,7 +18,7 @@ return {
 			{ "<leader>gt", function() package.loaded.gitsigns.toggle_deleted()             end, desc = "Toggle Deleted"                      },
 		},
 		---@type Gitsigns.Config
-		opts = { signcolumn = true },
+		opts = {},
 	},
 	{
 		-- For color highlighting
@@ -60,4 +60,39 @@ return {
 		},
 	},
 	{ "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, opts = {} },
+	{
+		"nvim-focus/focus.nvim",
+		version = "*",
+		opts = {},
+		init = function()
+			local ignore_filetypes = { "neo-tree" }
+			local ignore_buftypes = { "nofile", "prompt", "popup" }
+
+			local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+
+			vim.api.nvim_create_autocmd("WinEnter", {
+				group = augroup,
+				callback = function(_)
+					if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+						vim.w.focus_disable = true
+					else
+						vim.w.focus_disable = false
+					end
+				end,
+				desc = "Disable focus autoresize for BufType",
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = augroup,
+				callback = function(_)
+					if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+						vim.b.focus_disable = true
+					else
+						vim.b.focus_disable = false
+					end
+				end,
+				desc = "Disable focus autoresize for FileType",
+			})
+		end,
+	},
 }
