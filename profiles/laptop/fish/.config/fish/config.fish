2dbd58b342ec3680c60033f3f7a92d56a4e9acef
Rogue <rogue87@tuta.io>
2025-05-02 16:41:55 +0200

    cleanup fish config

--- profiles/laptop/fish/.config/fish/config.fish
+++ profiles/laptop/fish/.config/fish/config.fish
@@ -7,19 +7,10 @@ if status is-interactive
     fish_add_path ~/.cargo/bin
     fish_add_path ~/.spicetify
 
-    # check if mise exists
-    # if command -q -v mise >/dev/null
-    #     mise activate fish | source
-    # end
-
-
     # default editor
     set -x EDITOR nvim
     set -x VISUAL nvim
 
     # browser to be used by flutter
     set -x CHROME_EXECUTABLE chromium-browser
-	if command -q -v zoxide >/dev/null
-		zoxide init fish | source
-	end
 end
