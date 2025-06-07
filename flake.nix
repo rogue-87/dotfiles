26694b52e096e515c6b3b93c6bd2e0627d12f576
Rogue <rogue87@tuta.io>
2025-06-07 20:54:47 +0200

    switch from rustup to rust-overlay

--- flake.nix
+++ flake.nix
@@ -2,86 +2,106 @@
   description = "dotfiles flake(only for installing cli/tui tools)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay"; # rustup but nixified
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      rust-overlay,
+      flake-utils,
       ...
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages.${system}.default = pkgs.buildEnv {
-        name = "global pkgs";
-        paths = with pkgs; [
-          # nix
-          nixd
-          nixfmt-rfc-style
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
 
-          # lua
-          lua-language-server
-          lux-cli
-          selene
-          stylua
+        # manage rust installation
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          # components
+          extensions = [
+            "clippy"
+            "rust-analyzer"
+            "rust-src"
+          ];
+          # targets
+          # targets = [ ];
+        };
+      in
+      with pkgs;
+      {
+        packages.default = buildEnv {
+          name = "dotfiles-pkgs";
+          paths = [
+            # nix
+            nixd
+            nixfmt-rfc-style
 
-          # luau
-          luau
-          luau-lsp
-          lune
+            # rust
+            rust
+            rustlings
+            taplo
 
-          # rust
-          rustlings
-          rustup
-          taplo
+            # lua
+            lua-language-server
+            lux-cli
+            selene
+            stylua
 
-          # python
-          pyright
-          ruff
-          uv
+            # luau
+            luau
+            luau-lsp
+            lune
 
-          # java
-          gradle
-          jdt-language-server
-          maven
+            # python
+            pyright
+            ruff
+            uv
 
-          # webdev
-          bun
-          deno
-          prettierd
-          svelte-language-server
-          typescript-language-server
-          vscode-langservers-extracted
+            # java
+            gradle
+            jdt-language-server
+            maven
 
-          # text editor stuff
-          bash-language-server
-          fish-lsp
-          marksman
-          shfmt
-          tinymist
-          typst
-          yaml-language-server
+            # webdev
+            bun
+            deno
+            prettierd
+            svelte-language-server
+            typescript-language-server
+            vscode-langservers-extracted
 
-          # other
-          bat
-          du-dust
-          dua
-          eza
-          hurl
-          hyperfine
-          mask
-          mprocs
-          rusty-man
-          tokei
-          wiki-tui
-          wrkflw
-          xh
-          zoxide
-        ];
-      };
-    };
+            # text editor stuff
+            bash-language-server
+            fish-lsp
+            marksman
+            shfmt
+            tinymist
+            typst
+            yaml-language-server
+
+            # other
+            bat
+            du-dust
+            dua
+            eza
+            hurl
+            hyperfine
+            mask
+            mprocs
+            rusty-man
+            tokei
+            wiki-tui
+            wrkflw
+            xh
+            zoxide
+          ];
+        };
+      }
+    );
 }
