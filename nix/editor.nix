a634269c7ee6313f674647debe5c6761fc861d3a
Rogue <rogue87@tuta.io>
2025-07-11 15:52:01 +0200

    update pkgs

--- nix/editor.nix
+++ nix/editor.nix
@@ -1,14 +1,16 @@
-# stuff that neovim uses
+# stuff that I typically couple in with my nvim config
 { pkgs, ... }:
 with pkgs;
 [
   bash-language-server
+  # btop # use system package if you want gpu stats
   fish-lsp
+  lazydocker
+  lazygit
+  lazysql
   marksman
   shfmt
   tinymist
   typst
   yaml-language-server
-  lazygit
-  btop
 ]
