dd7b1f6401fff1bdaef7f18cf6b7abf074ec6a4b
Rogue <rogue87@tuta.io>
2025-07-11 15:57:40 +0200

    enable tailwind colors

--- profiles/global/nvim/.config/nvim/lua/plugins/editor/etc.lua
+++ profiles/global/nvim/.config/nvim/lua/plugins/editor/etc.lua
@@ -48,7 +48,7 @@ return {
 				mode = "background", -- Set the display mode.
 				-- Available methods are false / true / "normal" / "lsp" / "both"
 				-- True is same as normal
-				tailwind = false, -- Enable tailwind colors
+				tailwind = true, -- Enable tailwind colors
 				-- parsers can contain values used in |user_default_options|
 				sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
 				virtualtext = "󰝤",
