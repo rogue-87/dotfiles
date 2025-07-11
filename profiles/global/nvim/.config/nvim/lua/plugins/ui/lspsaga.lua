29d062071cee4b2b8ca086d32014a0e06c9094ea
Rogue <rogue87@tuta.io>
2025-07-11 15:56:56 +0200

    disable lsp symbols in winbar

--- profiles/global/nvim/.config/nvim/lua/plugins/ui/lspsaga.lua
+++ profiles/global/nvim/.config/nvim/lua/plugins/ui/lspsaga.lua
@@ -8,5 +8,6 @@ return {
 	opts = {
 		lightbulb = { sign = false, virtual_text = true },
 		rename = { in_select = false, keys = { quit = "<esc>" } },
+		symbol_in_winbar = { enable = false },
 	},
 }
