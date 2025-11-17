5de268779a5e895849a4648e425c51c3f0b58317
Rogue <rogue87@tuta.io>
2025-11-17 17:59:21 +0200

    make it loaded when actually needed

--- profiles/global/nvim/.config/nvim/lua/extra/collections/godot/init.lua
+++ profiles/global/nvim/.config/nvim/lua/extra/collections/godot/init.lua
@@ -1,8 +1,7 @@
-require("extra.collections.godot.lsp").setup()
-require("extra.collections.godot.dap").setup()
-require("extra.collections.godot.tools").setup()
-
 local projectfile = vim.fn.getcwd() .. "/project.godot"
 if vim.fn.filereadable(projectfile) == 1 then
+	require("extra.collections.godot.lsp").setup()
+	require("extra.collections.godot.dap").setup()
+	require("extra.collections.godot.tools").setup()
 	vim.fn.serverstart("./godothost")
 end
